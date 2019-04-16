7f214dc1af26fca12a4244211e58b7b4c0b16492
Javed Akhtar <akhtar.ja@gmail.com>
2019-04-15 18:05:16 -0700

    added a portfolio section, moved contact info to its own section, major CSS cleanup

--- app/gulpfile.js
+++ app/gulpfile.js
@@ -36,16 +36,6 @@ function modules() {
   ])
   .pipe(gulp.dest('./build/vendor/bootstrap'));
 
-  // Font Awesome
-  var fontAwesome = gulp.src([
-    './node_modules/font-awesome/**/*',
-    '!./node_modules/font-awesome/{less,less/*}',
-    '!./node_modules/font-awesome/{scss,scss/*}',
-    '!./node_modules/font-awesome/.*',
-    '!./node_modules/font-awesome/*.{txt,json,md}'
-  ])
-  .pipe(gulp.dest('./build/vendor/font-awesome'));
-
   // jQuery
   var jquery = gulp.src([
     './node_modules/jquery/dist/*',
@@ -59,18 +49,7 @@ function modules() {
   ])
   .pipe(gulp.dest('./build/vendor/jquery-easing'))
 
-  // Simple Line Icons
-  var simpleLineIconsFonts = gulp.src([
-    './node_modules/simple-line-icons/fonts/**',
-  ])
-  .pipe(gulp.dest('./build/vendor/simple-line-icons/fonts'))
-
-  var simpleLineIconsCss = gulp.src([
-    './node_modules/simple-line-icons/css/**',
-  ])
-  .pipe(gulp.dest('./build/vendor/simple-line-icons/css'))
-
-  return merge(bootstrap, fontAwesome, jquery, jqueryEasing, simpleLineIconsFonts, simpleLineIconsCss);
+  return merge(bootstrap, jquery, jqueryEasing);
 }
 
 // CSS task
