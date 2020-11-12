79745f61add6a785318d2958452c74134ad2c3ef
Javed Akhtar <akhtar.ja@gmail.com>
2020-11-11 16:55:35 -0800

    add prettierjs

--- now-playing/.serverless_plugins/NowPlayingDeploymentPlugin.js
+++ now-playing/.serverless_plugins/NowPlayingDeploymentPlugin.js
@@ -1,11 +1,11 @@
-const fs = require('fs');
-const AWS = require('aws-sdk');
+const fs = require("fs");
+const AWS = require("aws-sdk");
 
 class NowPlayingDeploymentPlugin {
   constructor(serverless) {
     this.hooks = {
-      "after:deploy:deploy": this.afterDeploy.bind(null, serverless)
-    }
+      "after:deploy:deploy": this.afterDeploy.bind(null, serverless),
+    };
   }
 
   afterDeploy(serverless) {
@@ -14,27 +14,34 @@ class NowPlayingDeploymentPlugin {
       const custom = serverless.service.custom;
       const service = serverless.service.service;
 
-      const credentials = new AWS.SharedIniFileCredentials({ profile: provider.profile });
+      const credentials = new AWS.SharedIniFileCredentials({
+        profile: provider.profile,
+      });
       const region = provider.region;
       const cloudFormation = new AWS.CloudFormation({ credentials, region });
 
       const params = {
-        StackName: `${service}-${custom.stage}`
+        StackName: `${service}-${custom.stage}`,
       };
-      let apiUrl = '';
+      let apiUrl = "";
 
       cloudFormation.describeStackResources(params, function (err, response) {
         if (err) {
           console.log(err, err.stack);
           return reject(err);
         } else {
-          const apiGatewayRestApi = response.StackResources.find(resource => resource.LogicalResourceId === "ApiGatewayRestApi").PhysicalResourceId;
-          if (custom.stage === 'prod' && provider.environment.API_WRAPPER_URL) {
+          const apiGatewayRestApi = response.StackResources.find(
+            resource => resource.LogicalResourceId === "ApiGatewayRestApi"
+          ).PhysicalResourceId;
+          if (custom.stage === "prod" && provider.environment.API_WRAPPER_URL) {
             apiUrl = `${provider.environment.API_WRAPPER_URL}/nowplaying`;
           } else {
             apiUrl = `https://${apiGatewayRestApi}.execute-api.${region}.amazonaws.com/${custom.stage}/nowplaying`;
           }
-          const fileContents = `const ${service.replace(/-/g, '_')}_apiurl = '${apiUrl}';`;
+          const fileContents = `const ${service.replace(
+            /-/g,
+            "_"
+          )}_apiurl = '${apiUrl}';`;
           const path = custom.config_path;
 
           if (!fs.existsSync(path)) {
